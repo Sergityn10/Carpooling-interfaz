f7c77b4ce034277aed134c12e06242cba4a1d1cd
Sergityn10 <sermarled@gmail.com>
2025-06-30 19:03:36 +0200

    Add new styles

---
+++ src/views/FrasesPersuasivas.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function FrasesPersuasivas() {
+  return (
+    <>
+              <div className='explicacion-tags-main'>
+        <section className='tag-main-container'>
+          <h3>¡Que no te pille la DGT!</h3>
+          <p>Incentivar la movilidad compartida no consiste en multarte, sino disfrutar del viaje y darte ayudas para moverte con facilidad. </p>
+          <p>¡Compartir coche en tus trayectos diarios no ha sido tan facil!</p>
+        </section>
+
+        <section className='tag-main-container'>
+          <img src="" alt="Imagen de un tren en svg o una avalancha en un bus o tren" />
+          <h3>¡Se acabaron las avalanchas en el transporte publico!</h3>
+          <p>Disfruta del comfort de ir en un vehículo privado a precio del transporte publico sin agobiarte de que el tren o bus llegue petado y no te puedas ni mover. </p>
+          <p>¡Además, es una forma de cuidar el medio ambiente!</p>   
+        </section>
+        <section className="tag-main-container">
+          <h3>Viaja seguro y tranquilo</h3>
+          <p>Para nosotros es muy importante conocer a nuestros usuarios. Por eso, examinamos atentamente las opiniones y los perfiles de nuestros usuarios para que sepas con quién vas a viajar. Puedes reservar tu próximo viaje con total seguridad y tranquilidad.</p>
+        </section>
+
+        
+      </div>
+      <section className='main-section'> 
+        <h2>¡Empieza a ahorrar haciendo lo de siempre!</h2>
+        <p>¿Tienes que ir a trabajar? ¿Vas a hacer la compra? ¿Tienes que ir a la universidad? ¡Comparte tu trayecto y ahorra dinero!</p>
+        <p>¡O si prefieres, busca un trayecto y ahorra dinero!</p>
+        <p>¡Es muy fácil! ¡Regístrate y empieza a compartir!</p>
+        <button>Publicar trayecto</button>
+        <button>Buscar trayecto</button>
+
+      </section>
+
+      <section className='main-section'>
+        <h2>¡Se acabaron las horribles esperas del transporte público!</h2>
+        <p>¿Estás cansado de esperar al autobús o al metro? ¡Comparte tu trayecto y ahorra tiempo!</p>
+        <p>Ya no es necesario que tengas que aguantar las multitudes ni las esperas infinitas del bus o metro.</p>
+        <p>¡Viaja tranquilo con nosotros!</p>
+        <button>Buscar trayectos</button>
+      </section>
+
+      <section className='main-section'>
+
+        <h2>¡Viaja seguro y con confianza!</h2> 
+        <p>¿Te preocupa tu seguridad? ¡No te preocupes! ¡Todos nuestros usuarios están verificados!</p>
+        <p>¡Además, puedes ver las opiniones de otros usuarios antes de compartir trayecto!</p>
+        <button>Registrarse</button>  
+      </section>
+    </>
+  )
+}
